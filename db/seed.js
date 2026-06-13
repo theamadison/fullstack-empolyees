5de03d62123a914dd4f584228115915ec4aa2200
theamadison <alfoxthea@gmail.com>
2026-06-13 13:24:37 -0400

    Add files via upload

--- db/seed.js
+++ db/seed.js
@@ -6,5 +6,12 @@ await db.end();
 console.log("🌱 Database seeded.");
 
 async function seedEmployees() {
-  // TODO
+  for (let i = 0; i < 15; i++) {
+    const employee = {
+      name: "Employee " + i,
+      birthday: "1001-10-01",
+      salary: Math.floor(Math.random() * 500000),
+    };
+    await createEmployee(employee);
+  }
 }
